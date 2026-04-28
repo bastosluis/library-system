a1d20be8eb5ee8bda80403a9dce3d81fae5358d0
Luis Fernando <lfb@ufc.br>
2026-04-28 11:57:31 -0300

    feat: add Book class with fields, accessors and one basic constructor

--- library-system/src/main/java/com/hiiragi/library/model/Book.java
+++ library-system/src/main/java/com/hiiragi/library/model/Book.java
@@ -1,5 +1,62 @@
 package com.hiiragi.library.model;
 
-public class Book {
+import java.time.Year;
+
+class Book {
+    private int id;
+    private String title;
+    private String isbn;
+    private String description;
+    private Year publicationYear;
+    // private Author author;
+    // private Category category;
+    // private List<BookCopy> copies;
     
+    public Book(int id, String title, String isbn, String description, Year publicationYear) {
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+        this.description = description;
+        this.publicationYear = publicationYear;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Year getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(Year publicationYear) {
+        this.publicationYear = publicationYear;
+    }
 }
