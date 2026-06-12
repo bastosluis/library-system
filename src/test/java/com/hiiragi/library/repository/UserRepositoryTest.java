ba1f84100c69d77d21abd4b7a321ba2b4b558808
Luis Fernando <lfb@ufc.br>
2026-06-12 14:34:22 -0300

    test: added UserRepository test suite

---
+++ src/test/java/com/hiiragi/library/repository/UserRepositoryTest.java
@@ -0,0 +1,130 @@
+package com.hiiragi.library.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.hiiragi.library.model.User;
+import static com.hiiragi.library.util.MockedObjects.createUser;
+/*
+Very similar to BookRepositoryTest.
+Both test classes are good candidates to refactoring 
+*/
+public class UserRepositoryTest {
+    
+    private UserRepository userRepo;
+
+    @BeforeEach
+    void setUp(){
+        userRepo = new UserRepository();
+    }
+
+
+    @Test
+    void shouldSaveUser(){
+        User user = createUser();
+        userRepo.save(user);
+        assertTrue(userRepo.findAll().contains(user));
+    }
+
+    @Test
+    void shouldSaveMultipleUsers(){
+        User user1 = createUser();
+        User user2 = createUser();
+        userRepo.save(user2);
+        userRepo.save(user1);
+        assertEquals(2, userRepo.findAll().size());    
+    }
+
+    @Test
+    void shouldReturnUserWhenIdExists(){
+        User user = createUser();
+        userRepo.save(user);
+        assertEquals(user.getId(), userRepo.findById(user.getId()).getId());
+    }
+
+    @Test
+    void shouldReturnAllUsers(){
+        User user = createUser();
+        userRepo.save(user);
+        assertEquals(user, userRepo.findAll().getFirst());
+    }
+
+    @Test
+    void shouldDeleteUser(){
+        User user = createUser();
+        userRepo.save(user);
+        userRepo.delete(user);
+        assertTrue(userRepo.isEmpty());
+    }
+
+    @Test
+    void shouldDeleteUserById(){
+        User user = createUser();
+        userRepo.save(user);
+        userRepo.deleteById(user.getId());
+        assertTrue(userRepo.isEmpty());
+    }
+
+    @Test
+    void shouldNotDeleteInexistentUserById() {
+        boolean deleted = userRepo.deleteById(999L);
+        assertFalse(deleted);
+    }
+
+    @Test
+    void shouldDeleteOnlySpecifiedUser(){
+        for (int i = 0; i < 10; i++){
+            userRepo.save(createUser());
+        }
+        User user = userRepo.findById(5L);
+        userRepo.delete(user);
+        assertEquals(null, userRepo.findById(5L));
+    }
+    
+    @Test
+    void shouldIncreaseSizeAfterSave(){
+        User user = createUser();
+        userRepo.save(user);
+        assertEquals(1, userRepo.findAll().size());
+    }
+
+    @Test
+    void shouldDecreaseSizeAfterDelete(){
+        User user = createUser();
+        userRepo.save(user);
+        userRepo.deleteById(user.getId());
+        assertEquals(0, userRepo.findAll().size());
+    }
+
+    @Test
+    void shouldExistByEmail(){
+        User user = createUser();
+        userRepo.save(user);
+        String email = user.getEmail();
+        assertTrue(userRepo.existsByEmail(email));
+    }
+
+    @Test
+    void shouldNotExistByEmailInEmptyRepo(){
+        assertFalse(userRepo.existsByEmail("should_not_exist"));
+    }
+
+    @Test
+    void shouldFindByName(){
+        User user = createUser();
+        userRepo.save(user);
+        String name = user.getName();
+        assertEquals(user, userRepo.findByName(name));
+    }
+
+    @Test
+    void shouldNotFindByName(){
+        User user = createUser();
+        userRepo.save(user);
+        assertNull(userRepo.findByName("should_not_exist"));
+    }
+}
